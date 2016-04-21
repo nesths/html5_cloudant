40ffcedf29b0b82b9c98b78af4d7471de605f1b2
gwc <gwcnesths@gmail.com>
2016-04-21 18:49:16 +0000

    support file upload

--- testdb.js
+++ testdb.js
@@ -8,10 +8,10 @@
     var initDB = function(){
 
         //for your own database, change "testdb" on the next line to your db name
-        var db = new PouchDB('https://nesths1.cloudant.com/<DATABASE_NAME>', {
+        var db = new PouchDB('https://nesths1.cloudant.com/testdb', {
             auth: {
-                username: '<API_KEY>',
-                password: '<API_PASSWORD>'
+                username: 'ineciesedifeasterethentl',
+                password: '9799a95f765427a3834fb4b169cbaa2e6409bdb9'
               }
         });
 
@@ -31,6 +31,9 @@
     // the main array for comments
     var comments = [];
 
+
+    var attachment = {}
+
     // initialize database
     var db = initDB();
 
@@ -41,7 +44,20 @@
         commentTable.empty();
         for (var i = comments.length-1; i>=0 ; i-- ) {
             var comment = comments[i];
-            var html = "<tr><td>" + comment.name + "</td><td>" + comment.comment + "</td></tr>"
+            var html = 
+            `
+                <tr><td>{{name}}</td><td>{{comment}}</td><td>{{attachment}}</td></tr>
+            `
+            html = html.replace("{{name}}", comment.name );
+            html = html.replace("{{comment}}", comment.comment)
+            
+            if( comment._attachments ){
+                html = html.replace("{{attachment}}", '<i class="material-icons prefix">attach_file</i>');
+                
+            }
+            else{
+                html = html.replace("{{attachment}}", "");
+            }
             commentTable.append(html);
         }
 
@@ -79,6 +95,11 @@
             comment: $('#message').val()
         }
 
+
+        if( attachment ){
+            entry._attachments = attachment;
+        }
+
         // insert to DB
         // update local comment array
         // clear form data
@@ -87,6 +108,10 @@
           .then(function (response) {
             comments.push(entry)
             commentForm.reset();
+            
+            // clear attachment
+            attachment = {};
+            
             renderComments();
 
           })
@@ -104,6 +129,27 @@
 
         // bind to HTML
         $("#formSubmit").click(submitForm);
+        
+        
+        $("#attachment")[0].addEventListener('change', function(){
+            var file = this.files[0];
+            // This code is only for demo ...
+            console.log("name : " + file.name);
+            console.log("size : " + file.size);
+            console.log("type : " + file.type);
+            console.log("date : " + file.lastModified); 
+            
+             var imageUrl =  URL.createObjectURL(file);
+             $("#imagePreview")[0].src = imageUrl;
+            
+            attachment = {
+                filename: {
+                    type: file.type,
+                    data: file
+                }
+            }
+        }, false);
+
 
     });
 
